8e13cbd463a0daf19dcb4fad5d0d914987c248c4
Revin Tanri <tanrirevin@gmail.com>
2021-08-21 07:41:27 +0800

    create GET route with eager loading: - fix error Association is not found between todolist and todolistItem - remove mysqlURI in server.js - import ./models/index.js to server.js - Add all 'db' object into models within server.js - create GET routes: todolist hasMany todolistItem

--- models/index.js
+++ models/index.js
@@ -1,30 +1,43 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
+// connecting database with sequelize
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
+// read our model folder
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
+  // for each of model file, they will import the model and created as object in db{} (line 9)
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
+// do the association
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
@@ -34,4 +47,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = db; //export db out to be used everywhere
--- server.js
+++ server.js
@@ -3,20 +3,45 @@ const express = require("express");
 const { Sequelize, DataTypes } = require("sequelize");
 const Todolist = require("./models/todolist");
 const TodolistItem = require("./models/todolistitem");
+const db = require("./models/index"); //bcos of no assoc is found, we need to use the Associate() that is called in models/todolists and /todolistitem so we need to import from index
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
-const mysqlURI = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASS}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DB}`;
-const sequelize = new Sequelize(mysqlURI);
+// const mysqlURI = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASS}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DB}`;
+// const sequelize = new Sequelize(mysqlURI);
 
-const TodolistModel = Todolist(sequelize, DataTypes);
-const TodolistItemModel = TodolistItem(sequelize, DataTypes);
+const TodolistModel = Todolist(db.sequelize, DataTypes);
+const TodolistItemModel = TodolistItem(db.sequelize, DataTypes);
+
+app.get("/api/v1/todolist/:todolistID", async (req, res) => {
+  // find todolist with associated todolist items
+  let todolist = null;
+  try {
+    todolist = await db.Todolist.findOne({
+      where: {
+        id: req.params.todolistID,
+      },
+      include: db.TodolistItem,
+    });
+    // # what if we do function above in Mysql
+    // SELECT * from todolists LEFT JOIN todolist_items ON todolists_items.todolist_id WHERE todolist.id = req.params.todolistID
+  } catch (err) {
+    res.statusCode = 500;
+    return res.json();
+  }
+
+  if (!todolist) {
+    res.statusCode = 404;
+    return res.json();
+  }
+  return res.json(todolist);
+});
 
 app.get("/api/v1/todolist", (req, res) => {
   // this will return a promise - need to do async await
-  const todolist = TodolistModel.findAll()
+  const todolist = db.Todolist.findAll()
     .then(resp => {
       return res.json(resp);
     })
@@ -33,7 +58,7 @@ app.post("/api/v1/todolist", (req, res) => {
 
   //   # Second Method
   //  create model object without persisting to database
-  const todolist = new TodolistModel();
+  const todolist = new db.Todolist();
   todolist.title = title;
 
   //   this is to save = persist to database
@@ -48,7 +73,7 @@ app.post("/api/v1/todolist/:todolistID/items", async (req, res) => {
   //   get the todolist from model
   let todolist = null;
   try {
-    todolist = await TodolistModel.findOne({
+    todolist = await db.Todolist.findOne({
       where: {
         id: req.params.todolistID,
       },
@@ -65,7 +90,7 @@ app.post("/api/v1/todolist/:todolistID/items", async (req, res) => {
   }
 
   //   create the todolist item
-  const item = new TodolistItemModel();
+  const item = new db.TodolistItem();
   item.todolist_id = todolist.id;
   item.title = req.body.title;
   if (req.body.description) {
